19c5c8b6e2a4234b8652e83e9e45afb18f3c207a
Leo Teixeira <teixeiraleo72@gmail.com>
2023-05-10 16:05:13 +0200

    :sparkles: add prisma model and one prisma request

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
 import mariadb from "mariadb";
+import { PrismaClient } from "@prisma/client";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
+const prisma = new PrismaClient();
+
+/* `const pool = mariadb.createPool({...})` is creating a connection pool to a MariaDB database. The
+`createPool` method takes an object with configuration options for the pool, including the host,
+user, password, database, and port to connect to. The pool allows multiple connections to be
+established to the database and reused as needed, improving performance and scalability of the
+application. */
 const pool = mariadb.createPool({
   host: "localhost",
   user: "root",
@@ -11,6 +20,11 @@ const pool = mariadb.createPool({
   port: 4000,
 });
 
+/* `const schema = buildSchema(` ... `);` is defining the GraphQL schema for the API. It is specifying
+the types of data that can be queried and the structure of those types. In this case, it defines
+types for `Visiteur`, `Ville`, `Style`, `Realise`, `Participe`, `Joue`, `Concert`, `Artiste`, and a
+`Query` type that includes functions to retrieve data for each of those types. The `buildSchema`
+function is used to create a GraphQL schema object from the schema definition string. */
 const schema = buildSchema(`
   type Visiteur {
     idVisiteur: Int!
@@ -72,81 +86,105 @@ const schema = buildSchema(`
   }
 `);
 
+// const root = {
+//   /* This is a resolver function for the `getArtistes` query in the GraphQL schema. It is using the
+// `pool` object from the `mariadb` library to establish a connection to a MySQL database and retrieve
+// all rows from the `Artiste` table. The `async/await` syntax is used to handle asynchronous
+// operations and the `try/catch/finally` block is used to handle errors and release the database
+// connection after the query is executed. The function returns the result of the query, which will be
+// an array of objects representing the rows in the `Artiste` table. */
+//   getArtistes: async () => {
+//     let conn;
+//     try {
+//       conn = await pool.getConnection();
+//       const result = await conn.query("SELECT * FROM Artiste");
+//       console.log(result);
+//       return result;
+//     } catch (err) {
+//       throw err;
+//     } finally {
+//       if (conn) {
+//         conn.release();
+//       }
+//     }
+//   },
+
+//   getConcerts: async () => {
+//     let conn;
+//     try {
+//       conn = await pool.getConnection();
+//       const result = await conn.query("SELECT * FROM Concert");
+//       return result;
+//     } catch (err) {
+//       throw err;
+//     } finally {
+//       if (conn) {
+//         conn.release();
+//       }
+//     }
+//   },
+
+//   getStyles: async () => {
+//     let conn;
+//     try {
+//       conn = await pool.getConnection();
+//       const result = await conn.query("SELECT * FROM Style");
+//       return result;
+//     } catch (err) {
+//       throw err;
+//     } finally {
+//       if (conn) {
+//         conn.release();
+//       }
+//     }
+//   },
+
+//   getVilles: async () => {
+//     let conn;
+//     try {
+//       conn = await pool.getConnection();
+//       const result = await conn.query("SELECT * FROM Ville");
+//       return result;
+//     } catch (err) {
+//       throw err;
+//     } finally {
+//       if (conn) {
+//         conn.release();
+//       }
+//     }
+//   },
+
+//   getVisiteurs: async () => {
+//     let conn;
+//     try {
+//       conn = await pool.getConnection();
+//       const result = await conn.query("SELECT * FROM Visiteur");
+//       return result;
+//     } catch (err) {
+//       throw err;
+//     } finally {
+//       if (conn) {
+//         conn.release();
+//       }
+//     }
+//   },
+// };
+
 const root = {
   getArtistes: async () => {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const result = await conn.query("SELECT * FROM Artiste");
-      console.log(result);
-      return result;
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) {
-        conn.release();
-      }
-    }
+    return prisma.artiste.findMany();
   },
-
   getConcerts: async () => {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const result = await conn.query("SELECT * FROM Concert");
-      return result;
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) {
-        conn.release();
-      }
-    }
+    return prisma.concert.findMany();
   },
-
   getStyles: async () => {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const result = await conn.query("SELECT * FROM Style");
-      return result;
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) {
-        conn.release();
-      }
-    }
+    return prisma.style.findMany();
   },
-
   getVilles: async () => {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const result = await conn.query("SELECT * FROM Ville");
-      return result;
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) {
-        conn.release();
-      }
-    }
+    return prisma.ville.findMany();
   },
-
   getVisiteurs: async () => {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const result = await conn.query("SELECT * FROM Visiteur");
-      return result;
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) {
-        conn.release();
-      }
-    }
+    return prisma.visiteur.findMany();
   },
 };
 
